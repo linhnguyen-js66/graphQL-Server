cc0076fe6dbef4b8a49b54d28c777afd1dae1950
Linh. Nguyen Ha - CMC Global DU1.3 <nhlinh3@cmcglobal.vn>
2022-07-13 17:22:36 +0700

    set up

---
+++ schema/schema.js
@@ -0,0 +1,27 @@
+import { gql } from "apollo-server-express";
+//type definition
+export const typeDefs =  gql`
+type Book {
+    id: ID
+    name: String
+    genre: String
+    author: Author
+}
+type Author {
+    id: ID!
+    name: String
+    age: Int
+    books: [Book]
+}
+# ROOT TYPE
+type Query {
+    books: [Book]
+    book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
+}
+type Mutation {
+    createAuthor(name: String, age: Int): Author
+    createBook(name: String, genre: String, authorId: ID!): Book
+}
+`
